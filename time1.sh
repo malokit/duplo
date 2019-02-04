24bb24ba7915d44061da09ca5d0c0e1b75b3b129
malokit <47317427+malokit@users.noreply.github.com>
2019-02-04 01:44:32 -0800

    Update time1.sh

--- time1.sh
+++ time1.sh
@@ -3,17 +3,17 @@ echo "Start"
 while (true) 
 do
  ./75.sh;
- sleep 180; 
+ sleep 170; 
   ./50.sh;
- sleep 210; 
+ sleep 220; 
    ./80.sh;
- sleep 300; 
+ sleep 270; 
    ./65.sh;
- sleep 210; 
+ sleep 180; 
    ./90.sh;
- sleep 180; 
+ sleep 200; 
   ./40.sh;
- sleep 180; 
+ sleep 190; 
 done;
 
 
